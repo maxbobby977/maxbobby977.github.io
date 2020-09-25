41e6f3e50aff12a82d8e085908f6a95a7f81318a
Max Bobby <maxbobby@college.harvard.edu>
2020-09-25 11:20:23 -0400

    Updates

---
+++ precache-manifest.069912580922776dca269f70be57535e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc794bbf61daec2f5dc039b3077b3a76",
+    "url": "/winniebday2/index.html"
+  },
+  {
+    "revision": "7fe8998367c4aeaa0d5c",
+    "url": "/winniebday2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "753db024b1c81ce77554",
+    "url": "/winniebday2/static/js/2.959d2a9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/winniebday2/static/js/2.959d2a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe8998367c4aeaa0d5c",
+    "url": "/winniebday2/static/js/main.a4a248b4.chunk.js"
+  },
+  {
+    "revision": "a12f95861abdd0d362bb",
+    "url": "/winniebday2/static/js/runtime-main.2e36027c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/winniebday2/static/media/logo.5d5d9eef.svg"
+  }
+]);
